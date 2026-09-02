72115973acdf61abaf0f8b9dddeb1b768fa157cc
homestuck-ng <blowjober1488@gmail.com>
2026-09-02 19:54:13 +0300

    Uploaded

--- external/make.sh
+++ external/make.sh
@@ -118,7 +118,7 @@ fi
 # ============================================================
 
 SAMLOADER_VENV="$OUT_DIR/tools/venv"
-SAMLOADER_EXEC="$SAMLOADER_VENV/bin/samloader-master"
+SAMLOADER_EXEC="$SAMLOADER_VENV/bin/samloader"
 
 if [ -x "$SAMLOADER_EXEC" ]; then
     SAMLOADER=false
