d520a75d574ab263135f4b50575d411a788da409
homestuck-ng <blowjober1488@gmail.com>
2026-09-03 11:09:53 +0300

    changed make_rom.sh

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -112,16 +112,19 @@ fi
 
 if $BUILD_TARGET_FILES || $BUILD_FLASHABLE_ZIP; then
     ZIP_FILE_NAME="${TARGET_CODENAME}_"
+
     if [ "$(GET_PROP "system" "ro.unica.version")" ]; then
         ZIP_FILE_NAME+="$(GET_PROP "system" "ro.unica.version")"
     else
         ZIP_FILE_NAME+="$ROM_VERSION"
     fi
+
     ZIP_FILE_NAME+="-target_files.zip"
 
     if [ ! -f "$OUT_DIR/$ZIP_FILE_NAME" ]; then
         LOG_STEP_IN true "Creating target-files zip"
-        "$SRC_DIR/scripts/internal/create_target_files_zip.sh" "$OUT_DIR/$ZIP_FILE_NAME" || exit 1
+        "$SRC_DIR/scripts/internal/create_target_files_zip.sh" \
+            "$OUT_DIR/$ZIP_FILE_NAME" || exit 1
         LOG_STEP_OUT
     else
         LOGW "File already exists: ${OUT_DIR//$SRC_DIR\//}/$ZIP_FILE_NAME"
@@ -129,7 +132,8 @@ if $BUILD_TARGET_FILES || $BUILD_FLASHABLE_ZIP; then
 
     if $BUILD_FLASHABLE_ZIP; then
         LOG_STEP_IN true "Creating flashable zip"
-        "$SRC_DIR/scripts/build_flashable_zip.sh" "$OUT_DIR/$ZIP_FILE_NAME" || exit 1
+        "$SRC_DIR/scripts/build_flashable_zip.sh" \
+            "$OUT_DIR/$ZIP_FILE_NAME" || exit 1
         LOG_STEP_OUT
     fi
 fi
